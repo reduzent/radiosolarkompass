d17b15ab8a6fdb7bef2e1b4ded32995997a5173d
Roman Haefeli <reduzent@gmail.com>
2011-05-02 18:04:51 +0200

    basically working now

--- datenerhebung/getradios.sh
+++ datenerhebung/getradios.sh
@@ -2,9 +2,10 @@
 
 function extract_stream_data {
   smid=$1
+  echo "Counter: $smid"
   readyforradiosolarkompass="false"
   wget --quiet -O /tmp/radio.html http://www.surfmusic.de/radio-station/xxx,${smid}.html
-  if [[ $(wc -l /tmp/radio.html | cut -f1 -d" ") > 1 ]]
+  if [ $(wc -l /tmp/radio.html | cut -f1 -d" ") -gt 1 ]
   then
     html2xhtml /tmp/radio.html -o /tmp/radio.xhtml
     sed -i -e 's/&nbsp;/ /g' /tmp/radio.xhtml
@@ -15,10 +16,15 @@ function extract_stream_data {
     #echo "City: $city"
     #echo "Country: $country"
     #echo "URL: $url"
-    $(pd -nomidi -noaudio -nrt -nogui -open getradios_checkstream.pd -send "streamurl http://www.netpd.org:8010/netpd.mp3" 2>&1 | grep readyforradiosolarkompass)
-    if [ $readyforradiosolarkompass = "true" ]
-    then
-      echo -ne "${name};${homepage};${city};${country};${url}\n"
+    if [ "$url" != "http://www.surfmusik.org/enostream.mp3" ]
+    then 
+      echo "Checking streamurl: $url ..."
+      $(pd -nomidi -noaudio -nrt -nogui -open getradios_checkstream.pd -send "streamurl $url" 2>&1 | grep readyforradiosolarkompass)
+      if [ $readyforradiosolarkompass = "true" ]
+      then
+        echo -ne "${smid};${name};${homepage};${city};${country};${url}\n" >> /home/roman/surfmusic_streamdata.csv
+        echo "STREAM OK ($name, $city, $country)"
+      fi
     fi
   fi
 }
