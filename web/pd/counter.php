47911b900e81db2eccb22c056cd883862777848c
Roman Haefeli <reduzent@gmail.com>
2011-06-18 13:10:39 +0200

    answer with status 500, if db statements fail

--- web/pd/counter.php
+++ web/pd/counter.php
@@ -6,24 +6,24 @@ opendb();
 if(isset($_POST['try'])) {
   $try_id = $_POST['try']; 
   $query = "SELECT `trycnt` FROM `radios` WHERE `id` = $try_id limit 1";
-  $result = mysql_query($query) or die ('Datenbankabfrage fehlgeschlagen');
+  $result = mysql_query($query) or header('HTTP/1.0 500 Internal Server Error');
   while(list($trycnt) = mysql_fetch_array($result)) {
     $trycnt += 1;
     /* echo $ort, $num_of_trial, "trial" ;*/
     $update_query = "UPDATE `radios` SET `trycnt` = $trycnt WHERE `id` = $try_id LIMIT 1 ";
-    mysql_query($update_query) or die ('Datenbankabfrage fehlgeschlagen');
+    mysql_query($update_query) or header('HTTP/1.0 500 Internal Server Error');
   }
 }
 
 if(isset($_POST['play'])) {
   $play_id = $_POST['play'] ; 
   $query = "SELECT `playcnt` FROM `radios` WHERE `id` = $play_id limit 1";
-  $result = mysql_query($query) or die ('Datenbankabfrage fehlgeschlagen');
+  $result = mysql_query($query) or header('HTTP/1.0 500 Internal Server Error');
   while(list($playcnt) = mysql_fetch_array($result)) {
     $playcnt += 1;
     /* echo $ort, $num_of_trial, "trial" ;*/
     $update_query = "UPDATE `radios` SET `playcnt` = $playcnt WHERE `id` = $play_id LIMIT 1 ";
-    mysql_query($update_query) or die ('Datenbankabfrage fehlgeschlagen');
+    mysql_query($update_query) or header('HTTP/1.0 500 Internal Server Error');
   }
 }
 
