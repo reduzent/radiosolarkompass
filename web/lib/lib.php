6a36791f5f0c1a4fc1ccb4b6b55f7ea518a33260
Roman Haefeli <reduzent@gmail.com>
2011-06-23 14:28:03 +0200

    added function to strip special chars and replace all special chars for the announcement

--- web/lib/lib.php
+++ web/lib/lib.php
@@ -110,9 +110,12 @@ function pdGetAnnounceData($radio_id) {
     where `radios`.`id` = '$radio_id'";
   $result = mysql_query($query) or header('HTTP/1.0 500 Internal Server Error');
   while(list($city, $country, $radio) = mysql_fetch_array($result)) {
-    echo "city $city;\n";
-    echo "country $country;\n";
-    echo "radio $radio;\n";
+    $city_clean = normalize_special_characters($city);
+    $country_clean = normalize_special_characters($country);
+    $radio_clean = normalize_special_characters($radio);
+    echo "city $city_clean;\n";
+    echo "country $country_clean;\n";
+    echo "radio $radio_clean;\n";
   }
 }
 
@@ -128,7 +131,7 @@ function generatePdPlaylist() {
     where  `radios`.`active` = true
     group by `cities`.`id`;
     ";
-  $result = mysql_query($query) or die header('HTTP/1.0 500 Internal Server Error');
+  $result = mysql_query($query) or header('HTTP/1.0 500 Internal Server Error');
   $timetable = array();
   while(list($id, $lat, $lon) = mysql_fetch_array($result)) {
     $sunrise_dec = calcSunriseTime($lat, $lon, 0);
@@ -571,6 +574,38 @@ function displayImportList() {
   }
   echo "</table>\n";
   echo "<input class=\"button\" type=\"submit\" value=\"Update / Delete\" />";
+}
+
+################# utils #################################################
+
+function normalize_special_characters( $str )
+{
+    # Quotes cleanup
+    $str = ereg_replace( chr(ord("`")), "'", $str );        # `
+    $str = ereg_replace( chr(ord("´")), "'", $str );        # ´
+    $str = ereg_replace( chr(ord("„")), ",", $str );        # „
+    $str = ereg_replace( chr(ord("`")), "'", $str );        # `
+    $str = ereg_replace( chr(ord("´")), "'", $str );        # ´
+    $str = ereg_replace( chr(ord("“")), "\"", $str );        # “
+    $str = ereg_replace( chr(ord("”")), "\"", $str );        # ”
+    $str = ereg_replace( chr(ord("´")), "'", $str );        # ´
+
+    $unwanted_array = array(    'Š'=>'S', 'š'=>'s', 'Ž'=>'Z', 'ž'=>'z', 'À'=>'A', 'Á'=>'A', 'Â'=>'A', 'Ã'=>'A', 'Ä'=>'A', 'Å'=>'A', 'Æ'=>'A', 'Ç'=>'C', 'È'=>'E', 'É'=>'E',
+                                'Ê'=>'E', 'Ë'=>'E', 'Ì'=>'I', 'Í'=>'I', 'Î'=>'I', 'Ï'=>'I', 'Ñ'=>'N', 'Ò'=>'O', 'Ó'=>'O', 'Ô'=>'O', 'Õ'=>'O', 'Ö'=>'O', 'Ø'=>'O', 'Ù'=>'U',
+                                'Ú'=>'U', 'Û'=>'U', 'Ü'=>'U', 'Ý'=>'Y', 'Þ'=>'B', 'ß'=>'Ss', 'à'=>'a', 'á'=>'a', 'â'=>'a', 'ã'=>'a', 'ä'=>'a', 'å'=>'a', 'æ'=>'a', 'ç'=>'c',
+                                'è'=>'e', 'é'=>'e', 'ê'=>'e', 'ë'=>'e', 'ì'=>'i', 'í'=>'i', 'î'=>'i', 'ï'=>'i', 'ð'=>'o', 'ñ'=>'n', 'ò'=>'o', 'ó'=>'o', 'ô'=>'o', 'õ'=>'o',
+                                'ö'=>'o', 'ø'=>'o', 'ù'=>'u', 'ú'=>'u', 'û'=>'u', 'ü'=>'u', 'ý'=>'y', 'ý'=>'y', 'þ'=>'b', 'ÿ'=>'y' );
+    $str = strtr( $str, $unwanted_array );
+
+    # Bullets, dashes, and trademarks
+    $str = ereg_replace( chr(149), "&#8226;", $str );    # bullet •
+    $str = ereg_replace( chr(150), "&ndash;", $str );    # en dash
+    $str = ereg_replace( chr(151), "&mdash;", $str );    # em dash
+    $str = ereg_replace( chr(153), "&#8482;", $str );    # trademark
+    $str = ereg_replace( chr(169), "&copy;", $str );    # copyright mark
+    $str = ereg_replace( chr(174), "&reg;", $str );        # registration mark
+
+    return $str;
 }
 
 ?>
