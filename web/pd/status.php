e2af33400eede794095e64d1c16b280fa02da152
Roman Haefeli <reduzent@gmail.com>
2011-06-25 23:14:59 +0200

    added 'online' status (that is updated every 20s to indicate that RSK is still online)

--- web/pd/status.php
+++ web/pd/status.php
@@ -1,19 +1,37 @@
 <?php
-
 include '../lib/lib.php';
 opendb();
-
+if(isset($_POST['t'])) {
+  $time = $_POST['t'];
+} else {
+  $time = 0;
+}
 if(isset($_POST['onair'])) {
   $id = $_POST['onair']; 
-  $query = "UPDATE `status` SET `value` = $id WHERE `param` = 'onair'";
+  $query = "
+     create temporary table `loeschi` 
+     select `value`, `playtime` 
+     from `status` 
+     where `param` = 'onair'";
+  mysql_query($query) or header('HTTP/1.0 500 Internal Server Error');
+  $query = "
+     replace into `status` 
+     (`param`, `value`, `playtime`) 
+     select 'played', `value`, `playtime` from `loeschi`
+     ";
+  mysql_query($query) or header('HTTP/1.0 500 Internal Server Error');
+  $query = "UPDATE `status` SET `value` = $id, `playtime` = sec_to_time($time) WHERE `param` = 'onair'";
   mysql_query($query) or header('HTTP/1.0 500 Internal Server Error');
 }
-
 if(isset($_POST['next'])) {
   $id = $_POST['next']; 
-  $query = "UPDATE `status` SET `value` = $id WHERE `param` = 'next'";
+  $query = "UPDATE `status` SET `value` = $id, `playtime` = sec_to_time($time) WHERE `param` = 'next'";
   mysql_query($query) or header('HTTP/1.0 500 Internal Server Error');
 }
-
+if(isset($_POST['online'])) {
+  $id = $_POST['online']; 
+  $query = "UPDATE `status` SET `value` = $id, `playtime` = sec_to_time($time) WHERE `param` = 'online'";
+  mysql_query($query) or header('HTTP/1.0 500 Internal Server Error');
+}
 closedb();
 ?>
