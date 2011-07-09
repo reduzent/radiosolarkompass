6645339d71aee31f1c6e7267a5bb5d12ec46f992
Roman Haefeli <reduzent@gmail.com>
2011-07-09 22:02:06 +0200

    remove unnecessary code

--- web/connect/js/rsk.stream.player.js
+++ web/connect/js/rsk.stream.player.js
@@ -7,19 +7,11 @@ $(document).ready(function(){
 
   $("#jquery_jplayer").jPlayer({
     ready: function () {
-      this.element.jPlayer("setFile", "http://195.176.254.176:8010/rsk.mp3").jPlayer("pause");
+      this.element.jPlayer("setFile", "http://195.176.254.176:8010/rsk.mp3").jPlayer("play");
       demoInstanceInfo(this.element, $("#demo_info")); // This displays information about jPlayer's configuration in the demo page
     },
     solution: "html, flash",
     swfPath: "js"
   })
-  .jPlayer("onProgressChange", function(loadPercent, playedPercentRelative, playedPercentAbsolute, playedTime, totalTime) {
-    jpPlayTime.text($.jPlayer.convertTime(playedTime));
-    jpTotalTime.text($.jPlayer.convertTime(totalTime));
-    demoStatusInfo(this.element, jpStatus); // This displays information about jPlayer's status in the demo page
-  })
-  .jPlayer("onSoundComplete", function() {
-    this.element.jPlayer("play");
-  });
 });
 
