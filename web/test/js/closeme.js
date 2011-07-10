013c1124c0870cfea7403691e4000f3cdd1941a2
Roman Haefeli <reduzent@gmail.com>
2011-07-10 11:25:55 +0200

    stand sonntag morgen

---
+++ web/test/js/closeme.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+
+function showstuff(boxid){
+   document.getElementById(boxid).style.visibility="visible";
+}
+
+function hidestuff(boxid){
+   document.getElementById(boxid).style.visibility="hidden";
+}
