32776bb41a264d166a176e3171f60ebd2582264f
Roman Haefeli <reduzent@gmail.com>
2011-07-10 11:58:08 +0200

    mark now functional and positions are displayed correctly

--- web/test/js/mark.animation.js
+++ web/test/js/mark.animation.js
@@ -1,11 +1,11 @@
-  var delay = 80;
-  var num = 4;
+  var delay = 120;
+  var num = 8;
   var current = 0;
   var urlbase = 'pix/aussenden_';
   function timeMsg()
   {
     var t=setTimeout("timeMsg()",delay);
-    current = (current + 1) % num;
+    current = (current + 1) % num + 1;
     document.getElementById('aussenden').src = urlbase + current + '.png';
   }
 
