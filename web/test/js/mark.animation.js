a7697753fdc4216d38ba59be7801fa3d70c35159
Roman Haefeli <reduzent@gmail.com>
2011-07-12 00:50:29 +0200

    only executde animation when #aussenden actually exists... avoids js errors on page load

--- web/test/js/mark.animation.js
+++ web/test/js/mark.animation.js
@@ -2,10 +2,13 @@
   var num = 8;
   var current = 0;
   var urlbase = 'pix/aussenden_';
-  function timeMsg()
-  {
+  var aussenden = document.getElementById("aussenden");
+  function timeMsg() {
     var t=setTimeout("timeMsg()",delay);
     current = (current + 1) % num + 1;
     document.getElementById('aussenden').src = urlbase + current + '.png';
+  }
+  if (aussenden != null) {
+    timeMsg();
   }
 
