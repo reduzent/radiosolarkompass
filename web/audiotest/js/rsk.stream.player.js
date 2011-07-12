e4dd471a7096ce84e7052a404f4e062eb6b11f2c
Roman Haefeli <reduzent@gmail.com>
2011-07-13 01:15:32 +0200

    remove audiotest.. not needed anymore

--- web/audiotest/js/rsk.stream.player.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-  $("#jquery_jplayer").jPlayer({
-    ready: function () {
-      this.element.jPlayer("setFile", "http://195.176.254.176:8010/rsk.mp3").jPlayer("play");
-    },
-    swfPath: "js"
-  })
-});
-
