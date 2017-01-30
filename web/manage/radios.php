d86c291f7cf18f3a1821f55432ef6b03e058edb4
Roman Haefeli <reduzent@gmail.com>
2017-01-30 09:50:19 +0100

    show number of distinct locations in title of radios.php

--- web/manage/radios.php
+++ web/manage/radios.php
@@ -32,6 +32,10 @@ list($total) = mysql_fetch_array($result);
 $query = "select count(*) from radios where active = true and operable = true";
 $result = mysql_query($query) or die('Datenbankabfrage fehlgeschlagen');
 list($active) = mysql_fetch_array($result);
+mysql_query("select SQL_CALC_FOUND_ROWS city_id from radios where operable = 1 group by city_id;");
+$query = "select FOUND_ROWS();";
+$result = mysql_query($query) or die('Datenbankabfrage fehlgeschlagen');
+list($number_of_cities) = mysql_fetch_array($result);
 
 ?>
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -42,7 +46,7 @@ list($active) = mysql_fetch_array($result);
 </head>
 <body>
 <?php switcher(); ?>
-<h4>Manage Radio Stations <?php echo "($active/$total)"; ?></h4>
+<h4>Manage Radio Stations <?php echo "($active/$total)"; ?> from <?php echo "$number_of_cities"; ?> locations</h4>
 <form name="manage_radios" action="radios.php" method="post">
 <?php
 displayStreamList();
