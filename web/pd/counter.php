0ed0e8dbecb0c8334f984cfb68af5dfb22a3041f
Roman Haefeli <reduzent@gmail.com>
2017-02-01 22:33:38 +0100

    reflect if anything changed in feedback

--- web/pd/counter.php
+++ web/pd/counter.php
@@ -3,33 +3,26 @@
 include '../lib/lib.php';
 opendb();
 
+$status = "failed";
 if(isset($_POST['try'])) {
   $try_id = $_POST['try']; 
   $query = "UPDATE `radios` set `trycnt` = `trycnt` + 1  WHERE `id` = $try_id";
   $result = mysql_query($query);
-  if ($result) {
+  if (mysql_affected_rows() == 1) {
     $status = "OK";
-  } else {
-    $status = "failed";
   }
-  $jsonarray =  array('status' => $status, 'data' => "");
-  $output = json_encode($jsonarray, JSON_PRETTY_PRINT);
-  echo $output;  
-}
-
-if(isset($_POST['play'])) {
+} elseif (isset($_POST['play'])) {
   $play_id = $_POST['play'] ; 
   $query = "UPDATE `radios` set `playcnt` = `playcnt` + 1  WHERE `id` = $play_id";
   $result = mysql_query($query);
-  if ($result) {
+  if (mysql_affected_rows() == 1) {
     $status = "OK";
-  } else {
-    $status = "failed";
   }
-  $jsonarray =  array('status' => $status, 'data' => "");
-  $output = json_encode($jsonarray, JSON_PRETTY_PRINT);
-  echo $output;  
 }
+
+$jsonarray =  array('status' => $status, 'data' => "");
+$output = json_encode($jsonarray, JSON_PRETTY_PRINT);
+echo $output;  
 
 closedb();
 ?>
