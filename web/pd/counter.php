4955acbd6f26f784e72af09f297a7e3a73efd22d
Roman Haefeli <reduzent@gmail.com>
2017-02-01 22:18:59 +0100

    make counter.php conformant to new JSON-based design

--- web/pd/counter.php
+++ web/pd/counter.php
@@ -5,26 +5,30 @@ opendb();
 
 if(isset($_POST['try'])) {
   $try_id = $_POST['try']; 
-  $query = "SELECT `trycnt` FROM `radios` WHERE `id` = $try_id limit 1";
-  $result = mysql_query($query) or header('HTTP/1.0 500 Internal Server Error');
-  while(list($trycnt) = mysql_fetch_array($result)) {
-    $trycnt += 1;
-    /* echo $ort, $num_of_trial, "trial" ;*/
-    $update_query = "UPDATE `radios` SET `trycnt` = $trycnt WHERE `id` = $try_id LIMIT 1 ";
-    mysql_query($update_query) or header('HTTP/1.0 500 Internal Server Error');
+  $query = "UPDATE `radios` set `trycnt` = `trycnt` + 1  WHERE `id` = $try_id";
+  $result = mysql_query($query);
+  if ($result) {
+    $status = "OK";
+  } else {
+    $status = "failed";
   }
+  $jsonarray =  array('status' => $status, 'data' => "");
+  $output = json_encode($jsonarray, JSON_PRETTY_PRINT);
+  echo $output;  
 }
 
 if(isset($_POST['play'])) {
   $play_id = $_POST['play'] ; 
-  $query = "SELECT `playcnt` FROM `radios` WHERE `id` = $play_id limit 1";
-  $result = mysql_query($query) or header('HTTP/1.0 500 Internal Server Error');
-  while(list($playcnt) = mysql_fetch_array($result)) {
-    $playcnt += 1;
-    /* echo $ort, $num_of_trial, "trial" ;*/
-    $update_query = "UPDATE `radios` SET `playcnt` = $playcnt WHERE `id` = $play_id LIMIT 1 ";
-    mysql_query($update_query) or header('HTTP/1.0 500 Internal Server Error');
+  $query = "UPDATE `radios` set `playcnt` = `playcnt` + 1  WHERE `id` = $play_id";
+  $result = mysql_query($query);
+  if ($result) {
+    $status = "OK";
+  } else {
+    $status = "failed";
   }
+  $jsonarray =  array('status' => $status, 'data' => "");
+  $output = json_encode($jsonarray, JSON_PRETTY_PRINT);
+  echo $output;  
 }
 
 closedb();
