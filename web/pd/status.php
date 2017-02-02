f6561739351310a123e1ffd1b42cc9343ac98a8a
Roman Haefeli <reduzent@gmail.com>
2017-02-02 15:16:18 +0100

    status.php: adapted to new RESTful design

--- web/pd/status.php
+++ web/pd/status.php
@@ -1,45 +1,68 @@
 <?php
 include '../lib/lib.php';
 opendb();
-if(isset($_POST['t'])) {
-  $time = $_POST['t'];
-} else {
-  $time = 0;
-}
+$status = "failed";
 if(isset($_POST['onair'])) {
   $id = $_POST['onair'];
   // move old 'onair' to 'played' 
   $query = "
-     create temporary table `loeschi` 
-     select `value`, `playtime` 
-     from `status` 
-     where `param` = 'onair'";
-  mysql_query($query) or header('HTTP/1.0 500 Internal Server Error');
-  $query = "
-     replace into `status` 
-     (`param`, `value`, `playtime`) 
-     select 'played', `value`, `playtime` from `loeschi`
+    update status s1, status s2 
+    set s1.value = s2.value,
+        s1.playtime = s2.playtime
+    where s1.param = 'played' 
+      and s2.param = 'onair'
      ";
-  mysql_query($query) or header('HTTP/1.0 500 Internal Server Error');
-  // update current 'onair' status
-  $query = "UPDATE `status` SET `value` = $id, `playtime` = sec_to_time($time) WHERE `param` = 'onair'";
-  mysql_query($query) or header('HTTP/1.0 500 Internal Server Error');
-  // write entry to log
-  $query = "insert into `onair_log` 
+  $result = mysql_query($query);
+  if ($result) {
+    // update current 'onair' status
+    $query = "
+      UPDATE `status` 
+      SET
+        `value` = $id,
+        `playtime` = now()
+      WHERE `param` = 'onair'";
+    mysql_query($query) and $status = "OK";
+    // write entry to log
+    $query = "
+      insert into `onair_log` 
       (`radio_id`, `onair_time`) 
       values 
-      ($id, concat(utc_date(), ' ', sec_to_time($time)))";
-  mysql_query($query) or header('HTTP/1.0 500 Internal Server Error');
-}
-if(isset($_POST['next'])) {
+      ($id, (
+        select playtime
+        from status 
+        where param = 'onair'
+        )
+      )";
+    mysql_query($query);
+  }
+} elseif (isset($_POST['next'])) {
   $id = $_POST['next']; 
-  $query = "UPDATE `status` SET `value` = $id, `playtime` = sec_to_time($time) WHERE `param` = 'next'";
-  mysql_query($query) or header('HTTP/1.0 500 Internal Server Error');
-}
-if(isset($_POST['online'])) {
+  $query = "
+    UPDATE `status` 
+    SET 
+      `value` = $id,
+      `playtime` = (
+        select ds.sunrise_time
+        from daily_schedule ds 
+        join radios r on r.city_id = ds.city_id 
+        where r.id = $id
+        )
+    WHERE `param` = 'next'";
+  mysql_query($query) and $status = "OK";
+} elseif (isset($_POST['online'])) {
   $id = $_POST['online']; 
-  $query = "UPDATE `status` SET `value` = $id, `playtime` = sec_to_time($time) WHERE `param` = 'online'";
-  mysql_query($query) or header('HTTP/1.0 500 Internal Server Error');
+  $query = "
+    UPDATE `status` 
+    SET
+      `value` = $id,
+      `playtime` = now() 
+    WHERE `param` = 'online'";
+  mysql_query($query) and $status = "OK";
 }
+
+$jsonarray =  array('status' => $status, 'data' => "");
+$output = json_encode($jsonarray, JSON_PRETTY_PRINT);
+echo $output;
+
 closedb();
 ?>
